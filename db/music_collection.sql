d9c765aeb0febd9e5dd21cb10a1568742c1cef37
davina <davina.kuhnel@gmail.com>
2018-04-25 14:58:38 +0100

    set up sql file

--- db/music_collection.sql
+++ db/music_collection.sql
@@ -3,11 +3,12 @@ DROP TABLE IF EXISTS albums;
 
 CREATE TABLE artists (
   id SERIAL4 PRIMARY KEY,
-  name VARCHAR(255),
+  name VARCHAR(255)
 );
 
 CREATE TABLE albums (
   id SERIAL4 PRIMARY KEY,
-  name VARCHAR(255),
-  
+  title VARCHAR(255),
+  genre VARCHAR(255),
+  artist_id INT4 REFERENCES artists(id)
 );
