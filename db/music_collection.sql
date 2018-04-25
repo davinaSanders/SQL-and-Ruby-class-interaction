9f411334d5ccd6d84a09041b77e018ab2431961c
davina <davina.kuhnel@gmail.com>
2018-04-25 14:46:27 +0100

    initial commit

---
+++ db/music_collection.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS artists;
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE artists (
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+);
+
+CREATE TABLE albums (
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  
+);
