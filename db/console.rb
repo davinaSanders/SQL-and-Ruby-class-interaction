e09b6e909d286061d11ee861f6763740e9c82cbb
davina <davina.kuhnel@gmail.com>
2018-04-25 15:12:03 +0100

    setup class

---
+++ db/console.rb
@@ -0,0 +1,5 @@
+require("pry")
+require_relative("../models/album.rb")
+require_relative("../models/artist.rb")
+
+artist
---
+++ models/album.rb
@@ -0,0 +1,12 @@
+require_relative("../db/sql_runner.rb")
+require_relative("artist.rb")
+
+class Album
+  attr_reader :id
+  attr_accessor :title, :genre
+  def initialize(options)
+    @id = options["id"].to_i if options["id"]
+    @title = options["title"]
+    @genre = options["genre"]
+    # @artist_id = options["artist_id"].to_i
+end
