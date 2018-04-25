705833fb8d97c8db80def2ff06464e0b30d53c9d
davina <davina.kuhnel@gmail.com>
2018-04-25 15:24:25 +0100

    objects created in console

--- db/console.rb
+++ db/console.rb
@@ -2,4 +2,10 @@ require("pry")
 require_relative("../models/album.rb")
 require_relative("../models/artist.rb")
 
-artist
+artist1 = Artist.new({"name" => "Joe"})
+artist2 = Artist.new({"name" => "Davina"})
+artist3 = Artist.new({"name" => "Luis"})
+
+album1 = Album.new({"title" => "Something", "genre" => "country", "artist_id" = artist1.id})
+album2 = Album.new({"title" => "Like", "genre" => "pop", "artist_id" = artist1.id})
+album3 = Album.new({"title" => "This", "genre" => "blues", "artist_id" = artist1.id})
